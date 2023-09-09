c5563cfb96d292713d3f21807c8314dc54bd2a31
Halil Tunc <hitunc@outlook.com>
2023-09-09 15:18:20 +0300

    feat: advert app add

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const { exec } = require("child_process");
 const mainapp = "main";
 const app1Path = "home";
 const app2Path = "product";
+const app3Path = "advert";
 
 const startApp1Command = `cd ${mainapp} && npm start`;
 const startApp2Command = `cd ${app1Path} && npm start`;
 const startApp3Command = `cd ${app2Path} && npm start`;
+const startApp4Command = `cd ${app3Path} && npm install`;
 
 function startApplication(command) {
   exec(command, (error, stdout, stderr) => {
@@ -21,3 +23,4 @@ function startApplication(command) {
 startApplication(startApp1Command);
 startApplication(startApp2Command);
 startApplication(startApp3Command);
+startApplication(startApp4Command);
