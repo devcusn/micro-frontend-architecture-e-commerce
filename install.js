fa31bed92d8d759fd42a30041f0735f61355a085
Halil Tunc <hitunc@outlook.com>
2023-09-09 14:39:14 +0300

    feat: installing script add

---
+++ install.js
@@ -0,0 +1,23 @@
+const { exec } = require("child_process");
+
+const mainapp = "main";
+const app1Path = "home";
+const app2Path = "product";
+
+const startApp1Command = `cd ${mainapp} && npm install`;
+const startApp2Command = `cd ${app1Path} && npm install`;
+const startApp3Command = `cd ${app2Path} && npm install`;
+
+function startApplication(command) {
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error installing dependency: ${error}`);
+    } else {
+      console.log(`Dependency installed: ${command}`);
+    }
+  });
+}
+
+startApplication(startApp1Command);
+startApplication(startApp2Command);
+startApplication(startApp3Command);
